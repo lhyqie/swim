92c9654b35800a209f8a8e5f326af003fbb2c2bb
lhyqie <lhymvp@gmail.com>
2024-03-09 18:00:30 -0800

    add bg color for qualified events

--- app.py
+++ app.py
@@ -76,10 +76,9 @@ def form():
 
 
 if  __name__ == '__main__':
+  port = 5000
   debug = True
   if debug:
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', 
                         level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
-    app.run(debug=debug)
-  else:
-    app.run()
+  app.run(debug=debug, port=port)
