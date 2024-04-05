7805f5e24a7bb6fac176bcc0e0d13621367432d0
lhyqie <lhymvp@gmail.com>
2024-04-05 13:17:48 -0700

    add function to clear table

--- init_db.py
+++ init_db.py
@@ -16,5 +16,13 @@ def create_db(db_file):
   connection.commit()
   connection.close()
 
-create_db('swimmers_test.db')
-create_db('swimmers.db')
+def clear_table(db_file):
+  connection = sqlite3.connect(db_file)
+  cur = connection.cursor()
+  cur.execute("DELETE FROM swimmers")
+  connection.commit()
+  connection.close()
+
+# create_db('swimmers_test.db')
+# create_db('swimmers.db')
+# clear_table('swimmers.db')
